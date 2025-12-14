a962a0bc9716c29017e6b34c4c3a12099a8207c0
liquiddominator <andresgoku1000@gmail.com>
2025-12-14 13:02:40 -0400

    ExternalApi arreglada

--- config/external_api.php
+++ config/external_api.php
@@ -12,6 +12,6 @@ return [
     |
     */
 
-    'orgtrack_url' => env('ORGTRACK_API_URL', 'http://192.168.56.1:8000'),
+    'orgtrack_url' => env('ORGTRACK_API_URL', 'https://orgtrack.dasalas.shop'),
 
 ];
